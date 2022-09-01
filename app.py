e22307d323100c42a006093efbc12053ab638124
Eduardo Fabricio <eduwill100@gmail.com>
2022-09-01 20:36:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def reply():
         orders.insert_one({"number":number,"item":select,"address":text,"order_time": datetime.now()})
         users.update_one({"number": number}, {"$set": {"status": "ordered"}})
         msg2 = res.message("Test")
-        msg2.media("https://i.ibb.co/F4TxF0G/rickroll.gif")
+        msg2.media("https://i.ibb.co/tqYmh9R/1628253583441.jpg")
     elif user['status'] == "ordered":
         res.message(
             "Oi, obrigado por nos contatar novamente *Salgados S.A*\nVocê pode escolher uma das opções abaixo\n\n*Digite o numero* correspodente:\n\n1️⃣  Para nos *contatar*\n2️⃣  Para *pedir* salgados\n3️⃣  Para saber o *horário de funcionamento*\n4️⃣  Para saber nosso *endereço*")
